b911e8f9f6384039f5540165aa2b1a229b8cfdc7
Luca Inghilterra <271539@studenti.unimore.it>
2024-09-20 17:25:09 +0200

    Data Manager creation Signed off by: Luca Inghilterra E-mail: 271539@studenti.unimore.it

--- src/data_loaders/case_id_extractor.py
+++ src/data_loaders/case_id_extractor.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from config.methods.configuration_loader import *
 from config.methods.json_loader import *
 
@@ -5,7 +6,7 @@ from config.methods.json_loader import *
 JSON_PATH_YAML = '../../config/files/json_path.yaml'
 GENE_ESPRESSION_JSON = 'gene_expression_json'
 METHYLATION_JSON = 'methylation_json'
-OVERALL_SURVIVAL = 'overall_survival_json'
+OVERALL_SURVIVAL_JSON = 'overall_survival_json'
 
 
 ## FUNCTIONS
@@ -15,15 +16,42 @@ def myprint(dictionary):
         print(i, element)
         i += 1
 
+def check_duplicates_case_ids(dictionary, json):
+    case_ids = []
+    for element in data[json]:
+        case_ids.append(element['case_id'])
+
+    from collections import Counter
+    duplicates = []
+    for element, count in Counter(case_ids).items():
+        if count > 1:
+            duplicates.append([element, count])
+    print("Duplicates case IDs:", duplicates)
+    print(len(duplicates))
+
+def check_singles_case_ids(dictionary, json):
+    case_ids = []
+    for element in data[json]:
+        case_ids.append(element['case_id'])
+
+    singles = []
+    for element, count in Counter(case_ids).items():
+        if count == 1:
+            singles.append([element, count])
+    print("Singles case IDs:", singles)
+    print(len(singles))
+
 
 ## MAIN
 if __name__ == "__main__":
 
-    data = {}
+    # Loading YAML file
     file_path = yaml_loader(JSON_PATH_YAML)
-    data[GENE_ESPRESSION_JSON] = json_loader(file_path[GENE_ESPRESSION_JSON])
-    data[METHYLATION_JSON] = json_loader(file_path[METHYLATION_JSON])
-    data[OVERALL_SURVIVAL] = json_loader(file_path[OVERALL_SURVIVAL])
+
+    # Storing data from JSON file
+    data = {GENE_ESPRESSION_JSON: json_loader(file_path[GENE_ESPRESSION_JSON]),
+            METHYLATION_JSON: json_loader(file_path[METHYLATION_JSON]),
+            OVERALL_SURVIVAL_JSON: json_loader(file_path[OVERALL_SURVIVAL_JSON])}
 
     # Storing only information about 'case_id', 'file_name' and 'file_id' for each JSON file
     for file in data:
@@ -36,11 +64,6 @@ if __name__ == "__main__":
     '''
     # FIRST CASE: Overall Survival based only on Gene Expression
     overall_survival_ids = {record['case_id'] for record in data['overall_survival_json']}
-
-
-
     # Filtra i record in gene_expression_json che hanno un case_id presente in overall_survival_json
     filtered_gene_expression = [record for record in data['gene_expression_json'] if record['case_id'] in overall_survival_ids]
-
-    
-        '''
+    '''
