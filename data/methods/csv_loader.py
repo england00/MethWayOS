a886752e357406af5ea2d0848c2d6c4253068563
Luca Inghilterra <271539@studenti.unimore.it>
2024-09-26 15:21:45 +0200

    Gene Expression and Overall Survival Binary Classification Signed off by: Luca Inghilterra E-mail: 271539@studenti.unimore.it

---
+++ data/methods/csv_loader.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import logging
+from config.methods.configuration_loader import yaml_loader
+from json_dir.methods.json_loader import json_loader
+from error.general_error import GeneralError
+
+
+def csv_loader(path, yaml_file, json_file_name):
+    """
+        :param path: CSV file to load
+        :param yaml_file: YAML file to load
+        :param json_file_name: index inside the JSON file to load
+        :return dataframe: information loaded from the CSV file
+        :return dataframe_columns: column names of data loaded from the CSV file
+    """
+    json_paths = yaml_loader(yaml_file)
+    names = json_loader(json_paths[json_file_name])
+    names.append('y')
+    try:
+        dataframe = pd.read_csv(path, delimiter=';', header=None, names=names)
+        print(f"Data has been correctly loaded from {path} file")
+        return dataframe, names
+    except FileNotFoundError as e:
+        logging.error(str(e))
+        raise GeneralError(f"{path} file doesn't exist") from None
+    except Exception as e:
+        logging.error(str(e))
+        raise GeneralError(f"a problem occurred while trying reading {path} file") from None
--- src/models/ge_&_os_binary_classification.py
+++ src/models/ge_&_os_binary_classification.py
@@ -1,5 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+from colorama import Fore
+from sklearn.model_selection import train_test_split
+from config.methods.configuration_loader import yaml_loader
+from data.methods.csv_loader import csv_loader
+
+
+## CONFIGURATION
+JSON_PATHS_YAML = '../../config/files/json_paths.yaml'
+DATASET_PATH_YAML = '../../config/files/dataset_paths.yaml'
+GENE_EXPRESSION = 'gene_expression'
+GENE_EXPRESSION_NAMES = 'gene_expression_names'
+
+
+## FUNCTIONS
+def title(text):
+    print(Fore.CYAN, '\n' + f' {text} '.center(80, '#'), Fore.RESET)
+
+
+## LOADING DATASET
+def loading_dataset(path):
+    # Loading COLUMNS
+    df, df_columns = csv_loader(path, JSON_PATHS_YAML, GENE_EXPRESSION_NAMES)
+
+    # Splitting dataset in TRAINING and TESTING
+    df_training, df_testing = train_test_split(df, test_size=0.2, random_state=42, shuffle=True)
+
+    return df_training, df_testing, df_columns
 
 
 ## MAIN
 if __name__ == "__main__":
-    print("ciao")
+    # Data Acquisition
+    title('DATA ACQUISITION')
+    dataset_paths = yaml_loader(DATASET_PATH_YAML)
+    training_set, testing_set, set_columns = loading_dataset(dataset_paths[GENE_EXPRESSION])
+
