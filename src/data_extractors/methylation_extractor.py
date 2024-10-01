b76713c02e88680bc29a89fca090369e33adbda0
Luca Inghilterra <271539@studenti.unimore.it>
2024-10-01 15:36:55 +0200

    Methylation Extractor Signed off by: Luca Inghilterra E-mail: 271539@studenti.unimore.it

--- src/data_extractors/methylation_extractor.py
+++ src/data_extractors/methylation_extractor.py
@@ -10,9 +10,9 @@ from logs.methods.log_storer import *
 JSON_PATHS_YAML = '../../config/files/json_paths.yaml'
 DIRECTORIES_PATHS_YAML = '../../config/files/directories_paths.yaml'
 DATASTORE_PATHS_YAML = '../../config/files/datastore_paths.yaml'
-GENE_EXPRESSION = 'gene_expression'
+METHYLATION = 'methylation'
 OVERALL_SURVIVAL = 'overall_survival'
-LOG_PATH = '../../logs/files/2.1 - GENE EXPRESSION Extractor.txt'
+LOG_PATH = '../../logs/files/3.1 - METHYLATION Extractor.txt'
 
 
 ## MAIN
@@ -33,42 +33,19 @@ if __name__ == "__main__":
     for dictionary in overall_survival_list:
         case_ids.append(dictionary['info']['case_id'])
 
+    print(len(case_ids))
+
     # Storing GENE EXPRESSION data from JSON file with 'case_id', 'file_name' and 'file_id' only for interested cases
-    gene_expression_list = json_loader(json_paths[GENE_EXPRESSION])
+    methylation_list = json_loader(json_paths[METHYLATION])
     buffer = []
-    for dictionary in gene_expression_list:
+    for dictionary in methylation_list:
         if dictionary['cases'][0]['case_id'] in case_ids:
-            buffer.append({'case_id': dictionary['cases'][0]['case_id'],
+            buffer.append({'case_id': dictionary['cases'][0]['case_id'], 
                            'file_name': dictionary['file_name'],
                            'file_id': dictionary['file_id']})
-    gene_expression_list = buffer
+    methylation_list = buffer
 
-    # Removing duplicates, taking only one case_id for each-one
-    buffer = []
-    case_ids = []
-    file_names = []
-    for dictionary in gene_expression_list:
-        if dictionary["case_id"] not in case_ids:
-            buffer.append(dictionary)
-            case_ids.append(dictionary["case_id"])
-            file_names.append(dictionary['file_name'])
-    gene_expression_list = buffer
-
-    # Searching only TSV files with the right 'case_id'
-    i = 0
-    gene_expression_datastore = []
-    for path in directory_loader(directories_paths[GENE_EXPRESSION]):
-        name = path.split('/')[len(path.split('/')) - 1]
-        if name in file_names:
-            for dictionary in gene_expression_list:
-                if name == dictionary['file_name']:
-                    i += 1
-                    gene_expression_datastore.append(tsv_loader(path, dictionary))
-                    break
-    print(f"Loaded {i} files")
-
-    # Storing the datastore inside a JSON file
-    json_storer(datastore_paths[GENE_EXPRESSION], gene_expression_datastore)
+    print(len(methylation_list))
 
     # Close LOG file
     sys.stdout = sys.__stdout__
