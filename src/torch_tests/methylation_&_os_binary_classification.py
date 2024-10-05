3f6a7ffbc6d1452f6a4550df4353d86b8e05b723
Luca Inghilterra <271539@studenti.unimore.it>
2024-10-05 15:08:05 +0200

    Methylation & Overall Survival Binary Classification Signed off by: Luca Inghilterra E-mail: 271539@studenti.unimore.it

---
+++ src/torch_tests/methylation_&_os_binary_classification.py
@@ -0,0 +1,72 @@
+from colorama import Fore
+from config.methods.configuration_loader import yaml_loader
+from src.binary_classification.functions.f1_dataset_acquisition import dataset_acquisition
+from src.binary_classification.functions.f2_exploratory_data_analysis import exploratory_data_analysis
+from src.binary_classification.functions.f3_features_preprocessing import features_preprocessing
+from src.binary_classification.functions.f4_feature_selection import feature_selection
+from src.binary_classification.functions.f5_dataset_splitting import dataset_splitting
+from src.binary_classification.functions.f6_models import models
+from src.binary_classification.functions.f7_grid_search import grid_search
+from src.binary_classification.functions.f8_cross_validation_model_assessment import cross_validation_model_assessment
+from src.binary_classification.functions.f9_training import training
+from src.binary_classification.functions.f10_testing import testing
+from logs.methods.log_storer import *
+
+
+## CONFIGURATION
+JSON_PATHS_YAML = '../../config/files/json_paths.yaml'
+DATASET_PATH_YAML = '../../config/files/dataset_paths.yaml'
+METHYLATION = 'methylation'
+METHYLATION_NAMES = 'methylation_names'
+LOG_PATH = '../../logs/files/3.3 - METHYLATION & OS - Binary Classification.txt'
+RANDOM_STATE = None  # if 'None' changes the seed to split training set and test set every time
+LOWER_THRESHOLD = 1500  # 730 (2 years)
+UPPER_THRESHOLD = 2500  # 2920 (8 years)
+PCA_DIMENSION = 80
+FEATURES_NUMBER = 20
+VERBOSE = False
+PLOT = False
+
+
+## FUNCTIONS
+def title(text):
+    print(Fore.CYAN, '\n' + f' {text} '.center(80, '#'), Fore.RESET)
+
+
+## MAIN
+if __name__ == "__main__":
+
+    # Open LOG file
+    logfile = open(LOG_PATH, 'w')
+    sys.stdout = DualOutput(sys.stdout, logfile)
+
+    # Data Acquisition
+    title('DATA ACQUISITION')
+    dataset_paths = yaml_loader(DATASET_PATH_YAML)
+    dataset, dataset_columns = dataset_acquisition(path=dataset_paths[METHYLATION],
+                                                   json_paths_yaml=JSON_PATHS_YAML,
+                                                   names=METHYLATION_NAMES)
+
+    # Exploratory Data Analysis
+    title('EXPLORATORY DATA ANALYSIS with RAW DATA')
+    exploratory_data_analysis(dataframe=dataset, verbose=VERBOSE, plot=PLOT)
+
+    # Feature Preprocessing
+    title('FEATURES PREPROCESSING')
+    dataset_columns.remove('y')
+    dataset = features_preprocessing(dataframe=dataset,
+                                     lower_threshold=LOWER_THRESHOLD,
+                                     upper_threshold=UPPER_THRESHOLD,
+                                     verbose=VERBOSE,
+                                     column_names=dataset_columns)
+
+    # Feature Selection
+    title('FEATURE SELECTION')
+    dataset = feature_selection(dataframe=dataset,
+                                rand_state=RANDOM_STATE,
+                                pca_dimension=PCA_DIMENSION,
+                                feature_number=FEATURES_NUMBER)
+
+    # Dataset Splitting
+    title('DATASET SPLITTING')
+    training_set, testing_set = dataset_splitting(dataframe=dataset, rand_state=RANDOM_STATE)
