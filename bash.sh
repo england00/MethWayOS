a974a3ee54b42ae85047aa9c4a53412a25cd5b93
england00 <271539@studenti.unimore.it>
2024-11-08 17:55:56 +0100

    AImage Lab Server's Update Signed off by: Luca Inghilterra E-mail: 271539@studenti.unimore.it

---
+++ bash.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Managing CUDA modules
+module unload cuda/12.1
+module load cuda/11.8
+
+# Sourcing Conda and Activating Environment
+. /usr/local/anaconda3/etc/profile.d/conda.sh
+conda activate MachineLearning
+
+# GB: original params were: --mem=10G --time 4:00:00
+srun -Q -wailb-login-02 --immediate=10 --mem=8G --partition=all_serial --gres=gpu:1 --nodes=1 --time 1:00:00 --pty --account=tesi_linghilterra bash
